6e985fff8d2540a3445893777039cd02ff1e42e0
DeGlazed <raducutanase@gmail.com>
2023-11-27 20:13:46 +0200

    add hotjar to site

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,14 @@ import { BrowserRouter } from "react-router-dom";
 import ReactDOM from "react-dom";
 import { I18nextProvider } from "react-i18next";
 import "antd/dist/antd.css";
+import Hotjar from '@hotjar/browser';
 
 import Router from "./router";
 import i18n from "./translation";
+
+const siteId = 3744646;
+const hotjarVersion = 6;
+Hotjar.init(siteId, hotjarVersion);
 
 const App = () => (
   <BrowserRouter>
